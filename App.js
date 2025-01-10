9c04504410f66185e758b94e2206d282fea38321
Maryam <103954444+Muserafatima@users.noreply.github.com>
2025-01-10 09:43:47 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Layout from './layout/Layout';
+
+
+
+const App = () => {
+  return (
+    
+    <>
+<Layout/>
+
+</>
+  );
+}
+
+
+
+export default App;
